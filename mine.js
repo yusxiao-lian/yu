114c875a4c07cdfe2fb87b62feccbd7d714567a1
yuxiao <1049146413@qq.com>
2019-11-09 21:05:58 +0800

    第三次提交

--- mine.js
+++ mine.js
@@ -21,3 +21,31 @@ this.getColorRgb = function(){
     var color = 'rgb('+r+','+g+','+b+')';
     return color;
 }
+
+
+function Mine(){ 
+    //获取区间随机整数
+    this.getInt = function(n,m){
+     var int = Math.floor(Math.random()*(m-n+1)) + n; 
+     return int;
+    } 
+    //获取随机rgb颜色
+    this.getColorRgb = function(){
+        var r = this.getInt(0,255);
+        var g = this.getInt(0,255);
+        var b = this.getInt(0,255);
+        var color = 'rgb('+r+','+g+','+b+')';
+        return color;
+    }
+    //获取16进制随机颜色 
+    this.getColorHex = function(){
+        var str = ['a','b','c','d','e','f','0','1','2','3','4','5','6','7','8','9'];
+        var color = ['#'];
+        for(i=6;i>0;i--){ 
+            var index =Math.floor(Math.random()*str.length);  //随机获取索引       
+            var rand = str[index];  //随机获得元素
+            color.push(rand);
+        }
+        return color.join('');
+    }                 
+}
